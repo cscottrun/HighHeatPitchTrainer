f2bc950f0d8a67796da20d52288c733576b566f5
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2019-01-22 10:57:50 -0600

    file structure and accelerometer

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Home from './components/Home'
-import Accelerometer from './components/Accelerometer'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        < Home />
+        <Text> Welcome to High Heat </Text>
       </View>
     );
   }
